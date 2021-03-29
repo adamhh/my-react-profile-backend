dc37926926b1a7e4e9d2c63a4d7c0fd9d1d9c0b6
Adam Hall <adamhh@uw.edu>
2021-03-28 19:37:38 -0700

    switching from Sendgrid to postmark

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const postMark = require('postmark');
 
 const app = express();
-const client = new postMark.Client(process.env.API_KEY);
+
 require("dotenv").config();
 
 app.use(bodyParser.json());
@@ -24,13 +24,7 @@ app.get("/api", (req, res, next) => {
 });
 
 app.post("/api/email", (req, res, next) => {
-    // sendGrid.setApiKey(process.env.API_KEY);
-    // const msg = {
-    //     to:"adamhhall93+website@gmail.com",
-    //     from: 'personalsiteinquiry@gmail.com',
-    //     subject: "Website Inquiry From:   " + req.body.email,
-    //     text: req.body.message
-    // }
+    const client = new postMark.Client(process.env.API_KEY);
     client.sendEmail({
         "From": "personalsiteinquiry@gmail.com",
         "To": "adamhhall93@gmail.com",
@@ -47,21 +41,6 @@ app.post("/api/email", (req, res, next) => {
                 success:false
             });
         })
-
-    // sendGrid.send(msg)
-    //     .then(result => {
-    //         res.status(200).json({
-    //             success: true
-    //         });
-    //     })
-    //
-    //     .catch(err => {
-    //         console.log("error", err);
-    //         res.status(401).json({
-    //             success: false
-    //         });
-    //
-    //     });
 });
 
 
